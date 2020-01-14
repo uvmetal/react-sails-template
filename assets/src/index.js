a70e3b2b72125d05ff63741d64882e9429081e47
Shane Mann <shane.mann@gmail.com>
2020-01-14 12:15:02 -0500

    init

---
+++ assets/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const App = () => {
+  return (
+    <h1>Sample React and Sails App</h1>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
